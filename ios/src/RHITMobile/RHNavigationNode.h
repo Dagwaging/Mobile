d4441402dee09bd5ef8eebd80eb8abe09a68cfbd
Jimmy Theis <jimmy@jetheis.com>
2011-09-19 01:37:22 -0400

    Update RHNavigationNode doc
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- ios/src/RHITMobile/RHNavigationNode.h
+++ ios/src/RHITMobile/RHNavigationNode.h
@@ -1,10 +1,24 @@
 //
 //  RHNavigationNode.h
-//  RHITMobile
+//  RHIT Mobile Campus Directory
 //
-//  Created by Jimmy Theis on 8/28/11.
-//  Copyright 2011 Rose-Hulman Institute of Technology. All rights reserved.
+//  Copyright 2011 Rose-Hulman Institute of Technology
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+/// Specialized RHNode that contains navigation-specific information, such
+/// as floor level and whether or not a point is indoors.
 
 #import "RHNode.h"
 
