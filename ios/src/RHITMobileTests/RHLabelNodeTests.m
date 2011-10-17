a4ea769948f56c90791023a116ff94fe8d238218
Jimmy Theis <jimmy@jetheis.com>
2011-10-17 17:56:48 -0400

    Change app name
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- ios/src/RHITMobileTests/RHLabelNodeTests.m
+++ ios/src/RHITMobileTests/RHLabelNodeTests.m
@@ -83,7 +83,7 @@
         storedNode = (RHLabelNode *) [results objectAtIndex:0];
         
         // Verify the properties set on our retrieved object
-        STAssertEquals(storedNode.latitude, latitude, @"Latitude is incorrect");
+        STAssertEquals(storedNode.latitude.doubleValue, latitude.doubleValue, @"Latitude is incorrect");
         STAssertEquals(storedNode.longitude.doubleValue, 0.0,
                        @"Longitude is incorrect");
     }
