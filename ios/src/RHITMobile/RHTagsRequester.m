658f0be0314b1b60629873fa968c09107be5f9db
Jimmy Theis <jimmy@jetheis.com>
2012-02-06 23:50:43 -0500

    Implement retrieval of tags
    refs #358
    
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- ios/src/RHITMobile/RHTagsRequester.m
+++ ios/src/RHITMobile/RHTagsRequester.m
@@ -18,6 +18,31 @@
 //
 
 #import "RHTagsRequester.h"
+#import "RHDataVersionManager.h"
+#import "RHTourTag.h"
+#import "RHTourTagCategory.h"
+#import "RHWebRequestMaker.h"
+
+#define kTagsPath @"/tours/tags"
+
+#define kRootKey @"Root"
+#define kChildrenKey @"Children"
+#define kTagsKey @"Tags"
+#define kNameKey @"Name"
+#define kIsDefaultKey @"IsDefault"
+
+
+@interface RHTagsRequester ()
+
+- (void)createManagedObjectsFromCategories:(NSArray *)categories
+                                inCategory:(RHTourTagCategory *)category
+                    inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
+
+- (void)createManagedObjectsFromTags:(NSArray *)tags
+                          inCategory:(RHTourTagCategory *)category
+              inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
+
+@end
 
 
 @implementation RHTagsRequester
@@ -37,7 +62,90 @@
 }
 
 - (void)updateTags {
+    // Only run on a background thread
+    if ([NSThread isMainThread]) {
+        [self performSelectorInBackground:@selector(updateTags) withObject:nil];
+        return;
+    }
     
+    // Check version before continuing
+    RHDataVersionManager *dataVersionManager = [RHDataVersionManager instance];
+    
+    if (!dataVersionManager.needsTagsUpdate) {
+        return;
+    }
+    
+    NSManagedObjectContext *localContext = [[NSManagedObjectContext alloc] init];
+    localContext.persistentStoreCoordinator = self.persistantStoreCoordinator;
+    
+    // Delete old tags and categories
+    NSFetchRequest *oldRequest = [NSFetchRequest fetchRequestWithEntityName:kRHTourItemEntityName];
+    NSArray *oldItems = [localContext executeFetchRequest:oldRequest error:nil];
+    
+    for (RHTourItem *tourTagItem in oldItems) {
+        [localContext deleteObject:tourTagItem];
+    }
+    
+    // Load new tags
+    NSDictionary *response = [RHWebRequestMaker JSONGetRequestWithPath:kTagsPath URLargs:@""];
+    NSDictionary *root = [response objectForKey:kRootKey];
+    
+    // Recursively save new tags and categories
+    [self createManagedObjectsFromTags:[root objectForKey:kTagsKey]
+                            inCategory:nil
+                inManagedObjectContext:localContext];
+    [self createManagedObjectsFromCategories:[root objectForKey:kChildrenKey]
+                                  inCategory:nil
+                      inManagedObjectContext:localContext];
+    
+    // Commit changes
+    NSError *saveError;
+    [localContext save:&saveError];
+    
+    if (saveError) {
+        NSLog(@"Problem saving new tags: %@", saveError);
+        return;
+    }
+    
+    // Notify delegate
+    [self.delegate performSelectorOnMainThread:@selector(didFinishUpdatingTags)
+                                    withObject:nil
+                                 waitUntilDone:NO];
+    
+    // Update tags version
+    [dataVersionManager upgradeTagsVersion];
+}
+
+#pragma mark - Private Methods
+
+- (void)createManagedObjectsFromCategories:(NSArray *)categories
+                                inCategory:(RHTourTagCategory *)category
+                    inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext {
+    for (NSDictionary *categoryDict in categories) {
+        RHTourTagCategory *newCategory = [NSEntityDescription insertNewObjectForEntityForName:kRHTourTagCategoryEntityName inManagedObjectContext:managedObjectContext];
+        
+        newCategory.name = [categoryDict objectForKey:kNameKey];
+        
+        [self createManagedObjectsFromTags:[categoryDict objectForKey:kTagsKey]
+                                inCategory:newCategory
+                    inManagedObjectContext:managedObjectContext];
+        [self createManagedObjectsFromCategories:[categoryDict objectForKey:kChildrenKey]
+                                      inCategory:newCategory
+                          inManagedObjectContext:managedObjectContext];
+    }
+    
+}
+
+- (void)createManagedObjectsFromTags:(NSArray *)tags
+                          inCategory:(RHTourTagCategory *)category
+              inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext {
+    for (NSDictionary *tagDict in tags) {
+        RHTourTag *newTag = [NSEntityDescription insertNewObjectForEntityForName:kRHTourTagEntityName 
+                                                          inManagedObjectContext:managedObjectContext];
+        newTag.name = [tagDict objectForKey:kNameKey];
+        newTag.isDefault = [tagDict objectForKey:kIsDefaultKey];
+        newTag.parent = category;
+    }
 }
 
 @end
