5cee15d68e577250181420b0879cceed5b3af21b
Kevin Wells <wellsie1116@gmail.com>
2012-04-11 11:10:19 -0400

    Add automatic firewall configuration

---
+++ server/SecureServer/README.txt
@@ -0,0 +1,54 @@
+
+# RHIT Mobile Secure Server
+
+## Installation Instructions
+
+ - Included with this readme is an installer (SecureMobileServerInstaller.msi).
+   Begin by installing the service using the installer.  The installer will
+   attempt to start the service and will setup the necessary exceptions in the
+   Windows firewall.
+
+ - Configure the connection string for the database and the path to read the
+   banner data from.  The configuration file is located in the application's
+   installation directory: 
+   C:\Program Files (x86)\Rose-Hulman\Secure Mobile Server\RHITMobileService.exe.config
+
+   The path that the service will import data from is located in the appSettings
+   section.  Replace the value specified in the "value" field with the
+   appropriate path to the data files.  This path may include network shares.
+
+   The connection string is located in the connectionStrings section.  A
+   connection string is simply a semicolon separated list of parameters
+   necessary to connect to the database.  Relevant fields include:
+
+    - Data Source: The address of the database server.  Connecting via TCP is of
+	               the form `tcp:address,port`
+	
+	- Initial Catalog: The name of the database to use.
+
+	- User Id: If the machine account cannot be used for authentication, a
+	  username must be specified here.
+	
+	- Password: If the machine account cannot be used for authentication, a
+	  password must be specified here.
+
+ - In order for configuration changes to take effect, the service needs to be
+   restarted, either through `services.msc` or via the command line:
+
+       $ net stop RHITMobileSecureServer
+       $ net start RHITMobileSecureServer
+
+ - After configuring the service, it is recommended to set the service to
+   automatically restart on failure.  This can be configured through
+   `services.msc`
+
+    - Open the properties for RHITMobileSecureServer
+	- Click the **Recovery** tab
+	- For each failure type, select "Restart the Service"
+	- Click OK
+
+## Monitoring
+
+The status of the server can be viewed through the window event viewer
+(`eventvwr.msc`) under the source name RHITMobileSecureServer.
+
--- server/SecureServer/RHITMobileService/ProjectInstaller.cs
+++ server/SecureServer/RHITMobileService/ProjectInstaller.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.ServiceProcess;
+using NetFwTypeLib;
 
 namespace RHITMobile.Secure
 {
@@ -24,20 +25,38 @@ namespace RHITMobile.Secure
             Installers.Add(process);
             Installers.Add(service);
 
+            AfterInstall += new InstallEventHandler(AfterInstall_ConfigureFirewall);
             AfterInstall += new InstallEventHandler(AfterInstall_StartService);
-            BeforeUninstall += new InstallEventHandler(BeforeUninstall_StopService);
+            //BeforeUninstall += new InstallEventHandler(BeforeUninstall_StopService);
         }
 
         public void BeforeUninstall_StopService(object sender, InstallEventArgs e)
         {
-            using (var sc = new ServiceController(WindowsService.SERVICE_NAME))
+            try
             {
-                if (sc.Status != ServiceControllerStatus.Stopped)
+                using (var sc = new ServiceController(WindowsService.SERVICE_NAME))
                 {
-                    sc.Stop();
-                    sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
+                    if (sc.Status != ServiceControllerStatus.Stopped)
+                    {
+                        sc.Stop();
+                        sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
+                    }
                 }
             }
+            catch (Exception)
+            { }
+        }
+
+        public void AfterInstall_ConfigureFirewall(object sender, InstallEventArgs e)
+        {
+            string path = service.Context.Parameters["AssemblyPath"];
+            if (path == null)
+                return;
+
+            FirewallManager firewall = new FirewallManager();
+
+            firewall.AuthorizeProgram("RHIT Secure Mobile Service", path,
+                NET_FW_SCOPE_.NET_FW_SCOPE_ALL, NET_FW_IP_VERSION_.NET_FW_IP_VERSION_ANY);
         }
 
         public void AfterInstall_StartService(object sender, InstallEventArgs e)
@@ -56,6 +75,48 @@ namespace RHITMobile.Secure
                 //{
                 //    throw new InvalidOperationException("Service failed to start in a reasonable amount of time");
                 //}
+            }
+        }
+
+        private class FirewallManager
+        {
+            private static INetFwMgr WinFirewallManager()
+            {
+                Type type = Type.GetTypeFromCLSID(
+                    new Guid("{304CE942-6E39-40D8-943A-B913C40C9CD4}"));
+                return Activator.CreateInstance(type) as INetFwMgr;
+            }
+
+            public void AuthorizeService(string title, string path, NET_FW_SCOPE_ scope, NET_FW_IP_VERSION_ ipver)
+            {
+                INetFwMgr mgr = WinFirewallManager();
+
+                Type type = Type.GetTypeFromProgID("HNetCfg.FwAuthorizedApplication");
+                INetFwAuthorizedApplication authapp = Activator.CreateInstance(type)
+                    as INetFwAuthorizedApplication;
+                authapp.Name = title;
+                authapp.ProcessImageFileName = path;
+                authapp.Scope = scope;
+                authapp.IpVersion = ipver;
+                authapp.Enabled = true;
+
+                mgr.LocalPolicy.CurrentProfile.AuthorizedApplications.Add(authapp);
+            }
+
+            public void AuthorizeProgram(string title, string path, NET_FW_SCOPE_ scope, NET_FW_IP_VERSION_ ipver)
+            {
+                INetFwMgr mgr = WinFirewallManager();
+                
+                Type type = Type.GetTypeFromProgID("HNetCfg.FwAuthorizedApplication");
+                INetFwAuthorizedApplication authapp = Activator.CreateInstance(type)
+                    as INetFwAuthorizedApplication;
+                authapp.Name = title;
+                authapp.ProcessImageFileName = path;
+                authapp.Scope = scope;
+                authapp.IpVersion = ipver;
+                authapp.Enabled = true;
+
+                mgr.LocalPolicy.CurrentProfile.AuthorizedApplications.Add(authapp);
             }
         }
     }
