fcceff51a963bcb03d7fcb4fc0f4043fa8cb047c
Jimmy Theis <jimmy@jetheis.com>
2012-04-28 14:28:48 -0400

    Hook up basics of login UI
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

---
+++ ios/src/RHITMobile/RHLoginViewController.m
@@ -0,0 +1,83 @@
+//
+//  RHLoginViewController.m
+//  Rose-Hulman Mobile
+//
+//  Copyright 2012 Rose-Hulman Institute of Technology
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "RHLoginViewController.h"
+
+
+@interface RHLoginViewController ()
+
+@property (nonatomic, strong) IBOutlet UITextField *usernameField;
+
+@property (nonatomic, strong) IBOutlet UITextField *passwordField;
+
+- (void)login;
+
+@end
+
+
+@implementation RHLoginViewController
+
+@synthesize usernameField = _usernameField;
+@synthesize passwordField = _passwordField;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table View Delegate Methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if (indexPath.section == 2) {
+        [self login];
+    }
+}
+
+#pragma mark - Private Methods
+
+- (void)login
+{
+    [self.usernameField resignFirstResponder];
+    [self.passwordField resignFirstResponder];
+}
+
+@end
